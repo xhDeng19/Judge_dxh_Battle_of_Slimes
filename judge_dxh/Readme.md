a8b11a94eee250dcabd48becd4cdfcbe25cd1c68
xhDeng19 <150785925+xhDeng19@users.noreply.github.com>
2024-07-02 14:15:04 +0800

    2024/07/02更新

--- judge_dxh/Readme.md
+++ judge_dxh/Readme.md
@@ -1,6 +1,6 @@
 # Judge_dxh 使用教程
 ### 制作 by 邓熙涵
-### 2024/06/30
+### 制作日期：2024/06/30
 
 ## 0.文件结构
 ```
@@ -8,26 +8,29 @@ judge_dxh.zip
 |- Project1
 |   |- task1
 |   |  |- judge_dxh.py
+|   |  |- Task1.exe
 |   |  |- data
 |   |     |- your_output_files
 |   |     |- 1.in
-|   |     |- 1.out
+|   |     |- 2.in
 |   |     |- ...
 |   |
 |   |- task2
 |   |  |- judge_dxh.py
+|   |  |- Task2.exe
 |   |  |- data
 |   |     |- your_output_files
 |   |     |- 1.in
-|   |     |- 1.out
+|   |     |- 2.in
 |   |     |- ...
 |   |
 |   |- task3
 |   |  |- judge_dxh.py
+|   |  |- Task3.exe
 |   |  |- data
 |   |     |- your_output_files
 |   |     |- 1.in
-|   |     |- 1.out
+|   |     |- 2.in
 |   |     |- ...
 ```
 ## 1.注意事项
@@ -35,20 +38,23 @@ judge_dxh.zip
 2. 提交作业时记得删掉该测评程序的**所有**文件。
 3. 测试时，需将所有的 .cpp 文件与所有的 .h 文件放到**同一个**文件夹下
 4. 每个task里面的data文件夹均**不**一样，不能替换成其他task的data文件夹。
-5. 每个task文件夹下的your_output_files文件夹用来存放每次运行后你的程序输出的结果，在首次运行前为**空**。
-6. 每个task文件夹下的 *.in 与 *.out 文件为测试所用案例，案例为同学给出，如果有错误或者有好的案例请联系 DanielDeng12321@163.com 。
+5. 每个data文件夹下的your_output_files文件夹用来存放每次运行后你的程序输出的结果，在首次运行前为**空**。
+6. 每个data文件夹下的 `*.in` 文件为测试所用案例，均用 `data generator生成`。运行程序后每个data文件夹下的 `*.out` 文件为官方提供的二进制程序的输出结果（注意**没有**第一次slime的血量输出）。
 7. 此程序提供的案例的结果必须是**获胜**、**平局**或**失败**，无法评测一个尚未终止的对局。
+8. 此程序不会检测**第一次slime的血量输出**，因二进制程序没有第一次slime的血量输出，无法保证案例的准确性。
+9. 如果有错误或者有好的案例请联系 DanielDeng12321@163.com 。
 
 ## 2.使用说明
-**以task1为例**
-1. 将 `judge_dxh/Project1/task1` 下的**全体**移动至你的 `P1-姓名-任务1` 文件夹下，如下
+**以task3为例**
+1. 将 `judge_dxh/Project1/task3` 下的**全体**移动至你的 `P1-姓名-任务3` 文件夹下，如下
 ```
-P1-姓名-任务1
+P1-姓名-任务3
 |- judge_dxh.py
+|- Task3.exe
 |- data
 |  |- your_output_files
 |  |- 1.in
-|  |- 1.out
+|  |- 2.in
 |  |- ...
 |
 |- main.cpp
@@ -56,16 +62,20 @@ P1-姓名-任务1
 |- *.h
 |- ...
 ```
-1. 在**当前目录**打开cmd，输入 `python judge_dxh.py` 并回车，`P1-姓名-任务1` 文件夹变为如下
+1. 在**当前目录**打开cmd，输入 `python judge_dxh.py` 并回车，`P1-姓名-任务3` 文件夹变为如下
 ```
-P1-姓名-任务1
+P1-姓名-任务3
 |- judge_dxh.py
+|- Task3.exe
 |- data
 |  |- your_output_files
 |     |- 1.out
+|     |- 2.out
 |     |- ...
 |  |- 1.in
 |  |- 1.out
+|  |- 2.in
+|  |- 2.out
 |  |- ...
 |
 |- main.cpp
@@ -74,7 +84,8 @@ P1-姓名-任务1
 |- ...
 ```
 * 若cmd输出 `1.out is correct`，则证明你的程序**成功编译**且第一个案例**完全正确**
-* 若cmd输出 `1.out is WRONG. Compare data/1.out and data/your_output_files/1.out to find out."`，则证明你的程序**成功编译**但第一个案例**输出有误**
+* 若cmd输出 `1.out is WRONG. Compare data/1.out and data/your_output_files/1.out to find out."`，则证明你的程序**成功编译**但第一个案例**输出有误**，`cmd窗口`中会显示具体的错误和错误所在的行数，如下（案例来自一个复活的slime的血量均为50的小朋友）
+<img src="..\\pic\\cmd_wrong_answer.png">
 * 若cmd输出其他提示，则证明你的程序**编译失败**或者命令行输入有误，请检查你的文件是否完好，你的程序是否含有bug，你当前的目录是否正确
 
 1. 如果你的程序成功编译但**输出有误**，有可能是程序**运行超时**，请检查是否有不合理的 while 循环。更改 judge_dxh.py 程序里的 `TIMEOUT = 2` 语句（单位为**秒**）**没有**实质性作用，因为提供的测试案例都是达到**获胜**、**平局**或**失败**的对局。
--- judge_dxh/task2/judge_dxh.py
+++ judge_dxh/task2/judge_dxh.py
@@ -16,6 +16,15 @@ def get_data_num(file_dir):
     index_name = file_dir.rfind('.')
     return int(file_dir[index_dir + 1: index_name])
 
+def output_authentic_files(task_files, data_out_files, data_in_files):
+    for i in range(len(data_in_files)):
+        with open(data_in_files[i], 'r') as input_file, open(data_out_files[i], 'w') as output_file:
+            # 输入为 data/*.in 输出为 data/your_output_files.out
+            try:
+                subprocess.run([task_files[0]], stdin=input_file, stdout=output_file, stderr=subprocess.PIPE, text=True, timeout=TIMEOUT)
+
+            except subprocess.TimeoutExpired:
+                print(f"{i + 1}.in is invalid!")
 
 def output_your_files(data_in_files, your_out_files):
     for i in range(len(data_in_files)):
@@ -28,20 +37,54 @@ def output_your_files(data_in_files, your_out_files):
                     print(run_result.stderr)
             except subprocess.TimeoutExpired:
                 continue
-            
+
+def modify_authentic_output_files(data_out_files):
+    for i in range(len(data_out_files)):
+        with open(data_out_files[i], 'r') as read_file:
+            new_line = []
+            lines = read_file.readlines()
+            for line in lines:
+                #替换Task?.exe中不合理的输出
+                new_line += line.replace("pet", "slime")\
+                            .replace("starts", "start")\
+                            .replace("You sends", "You send")\
+                            .replace("Enemy start", "Enemy starts")\
+                            .replace("Battle start!", "Battle starts!")\
+                            .replace("Enemy WIN! You LOSE!", "You Lose\n")\
+                            .replace("You WIN! Enemy LOSE!", "You Win\n")
+        
+        with open(data_out_files[i], "w") as write_file:
+            write_file.writelines(new_line)
+
+def del_first_hp_print(your_content):
+    for i in range(len(your_content)):
+        if "||" in your_content[i]:
+            del your_content[i]
+            return
 
 def compare_files(data_out_files, your_out_files):
     for i in range(len(data_out_files)):
         # 比较 data/*out.out 与 data/your_output_files.out
         with open(data_out_files[i], 'r') as fstandard, open(your_out_files[i], 'r') as fyours:
-            standard_content = fstandard.read()
-            your_content = fyours.read()
+            standard_content = fstandard.readlines()
+            your_content = fyours.readlines()
+            del_first_hp_print(your_content) #放弃检测第一次hp输出（Task?.exe的bug）
+            isCorrect = True
 
-            if (standard_content == your_content):
+            for line_i in range(min(len(standard_content), len(your_content))):
+                if standard_content[line_i] != your_content[line_i]:
+                    if isCorrect:
+                        print(f"{i + 1}.out is WRONG. Compare data\\{i + 1}.out and data\\your_output_files\\{i + 1}.out to find out.")
+                        print("\nDetail:") 
+                    print(f"line {line_i + 1}: ")
+                    print(f"Correct answer: {standard_content[line_i]}")
+                    print(f"Your answer: {your_content[line_i]}")
+                    print("\n")
+                    isCorrect = False
+
+            if (isCorrect):
                 print(f"{i + 1}.out is correct.")
-            else:
-                print(f"{i + 1}.out is WRONG. Compare data\\{i + 1}.out and data\\your_output_files\\{i + 1}.out to find out.")   
-
+                  
 
 
 # 文件目录               
@@ -49,6 +92,7 @@ data_in_files = glob.glob(in_current_dir('data\\*.in'))
 data_out_files = [in_current_dir(f'data\\{i + 1}.out') for i in range(len(data_in_files))]
 your_out_files = [in_current_dir(f'data\\your_output_files\\{i + 1}.out') for i in range(len(data_in_files))]
 cpp_files = glob.glob(in_current_dir('*.cpp'))
+task_files = glob.glob(in_current_dir('Task?.exe'))
 
 data_in_files.sort(key=lambda string : get_data_num(string))
 data_out_files.sort(key=lambda string : get_data_num(string))
@@ -67,6 +111,9 @@ else:
         print(compile_result.stderr)
     else:
         #编译成功
+        output_authentic_files(task_files, data_out_files, data_in_files)
+        modify_authentic_output_files(data_out_files)
+
         output_your_files(data_in_files, your_out_files)
         compare_files(data_out_files, your_out_files)
 
